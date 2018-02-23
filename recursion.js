7d56961c047436651daa1f445ae4f3f6b8811a89
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-02-22 21:31:33 -0600

    Jonathan Reeves and Shaun Kelly Completed JAVASCRIPT-II-MINI

--- recursion.js
+++ recursion.js
@@ -1,33 +1,48 @@
 // to test these problems you can run 'node recursion.js' in your terminal
 // Problem 1:
 
-let n = 1;
+/*let n = 1;
 while (n <= 10) {
   console.log('While Loop', n);
   n++;
-}
+}*/
 
 // write a recursive - function called countToTen that mimics the while loop above.
+let countToTen = function(value) {
+  if (value > 1) {
+    console.log(value);
+    return countToTen(value - 1);
+  } else {
+    return value;
+  }
+};
 
-// code here
 
-// when you code is ready, un-comment the next line and run the file
-// console.log(countToTen());
+// when your code is ready, un-comment the next line and run the file
+console.log(countToTen(10));
 /* ================ Next Problem ================= */
 
 // Problem 2:
 
 const factorial = n => {
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
+let result = 1;
+for (let i = 2; i <= n; i++) {
+  result *= i;
+}
+return result;
 };
 
 console.log(factorial(5));
 
 // write the above function in a recursive way.
 
+const recursiveFactorial = n => {
+  let result = n - 1;
+  if (n === 1) {
+      return n;
+  }
+  return n * recursiveFactorial(result)
+};
+
 // when your code is ready, un-comment the next line and run the file
-// console.log(recursiveFactorial());
+console.log(recursiveFactorial(4));
